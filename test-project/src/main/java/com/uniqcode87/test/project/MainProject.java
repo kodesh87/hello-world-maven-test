d87c56829c2884f81e1bf6ad146ddcd7b1a46342
uniqcode87 <54540612+uniqcode87@users.noreply.github.com>
2022-06-01 20:55:21 +0700

    create 2 project, one as library project, one as test project

---
+++ test-project/src/main/java/com/uniqcode87/test/project/MainProject.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uniqcode87.test.project;
+
+import com.uniqcode87.library.project.StringContainer;
+
+/**
+ *
+ * @author uniqcode87
+ */
+public class MainProject {
+    
+    public static void main(String args[]) {
+        StringContainer container = new StringContainer();
+        System.out.println(container.getMessage());
+    }
+}
---
+++ test-project/src/test/java/com/uniqcode87/test/project/StringContainerTest.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uniqcode87.test.project;
+
+import com.uniqcode87.library.project.StringContainer;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author uniqcode87
+ */
+public class StringContainerTest {
+    
+    @Test
+    @DisplayName("hello world")
+    void testMessage() {
+        StringContainer container = new StringContainer();
+        assertEquals("hello world", container.getMessage(), "matched");
+    }
+}
